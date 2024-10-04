84570db7e073c6cd54a26f876f6e1bd7d077f5f5
Jayce <kendump666@gmail.com>
2024-10-04 22:57:40 +0800

    Update theme-toggle.tsx

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,37 +1,47 @@
-"use client";
+"use client"
 
-import * as React from "react";
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
+import { useState } from "react"
+import { signIn } from "next-auth/react"
+import { toast } from "react-hot-toast"
 
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { Icons } from "@/components/icons"
+import { Button } from "@/components/ui/button"
 
-export function ModeToggle() {
-  const { setTheme } = useTheme();
+const LoginButton = () => {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true)
+    try {
+      await signIn("google")
+    } catch (error) {
+      toast.error(
+        error instanceof Error ? error.message : "Something went wrong"
+      )
+    } finally {
+      setTimeout(() => setIsLoading(false), 2500)
+    }
+  }
 
   return (
-    <div className="min-h-screen">
-        {isVisible && <Toast variant="success" message="Login Successfully"/>}
-        <div className="container flex flex-col justify-center items-center min-h-screen">
-            <h1 className="text-[2rem] lg:text-[3rem]">Who is Watching?</h1>
-            <Link to="/home" onClick={() => localStorage.removeItem('loggedUser')} className="flex flex-col items-center justify-center group">
-                <img
-                  src={photoURL ? photoURL : "/images/default-red.png"}
-                  alt="profile1"
-                  className="rounded-md w-8 lg:w-10 my-1 lg:my-2 group-hover:border-[2px] group-hover:border-white"
-                />
-                <h2 className="text-light text-[1.1rem] lg:text-[1.5rem] group-hover:text-white ">{name || email}</h2>
-            </Link>
-        </div>
-
-    </div>
+    <Button
+      aria-label="Login with Google"
+      variant="brand"
+      className="w-full"
+      onClick={isLoading ? undefined : loginWithGoogle}
+      disabled={isLoading}
+    >
+      {isLoading ? (
+        <Icons.spinner
+          className="mr-2 h-4 w-4 animate-spin"
+          aria-hidden="true"
+        />
+      ) : (
+        <Icons.google className="mr-2 h-4 w-4" aria-hidden="true" />
+      )}
+      Google
+    </Button>
   )
 }
 
-export default Profiles
+export default LoginButton
