316cc523bafdb5e16bf03845b9236b93adf03f17
Jayce <kendump666@gmail.com>
2024-10-04 22:46:53 +0800

    Update theme-toggle.tsx

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,40 +1,53 @@
 "use client";
 
 import * as React from "react";
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
+import { useEffect, useState } from "react"
+import { Link, useLocation } from "react-router-dom"
+import Toast from "../Toast/Toast"
 
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+const Profiles = () => {
+  const [photoURL, setPhotoURL] = useState(null)
+  const [name, setName] = useState(null)
+  const [email, setEmail] = useState(null)
+  const [isVisible, setIsVisible] = useState(false)
 
-export function ModeToggle() {
-  const { setTheme } = useTheme();
+  useEffect(() => {
+    window.scrollTo(0,0)
+    const userData = JSON.parse(localStorage.getItem('userData'))
+    const isLoggedFirst = JSON.parse(localStorage.getItem('loggedUser'))
+
+    setPhotoURL(userData?.photoURL)
+    setName(userData?.displayName)
+    setEmail(userData?.email)
+
+    if(isLoggedFirst){
+      setIsVisible(true)
+    }
+
+    const timeout = setTimeout(() => {
+      setIsVisible(false)
+    }, 2000)
+
+    return () => clearTimeout(timeout)
+  }, [])
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button className="hover:bg-transparent" variant="ghost" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
+    <div className="min-h-screen">
+        {isVisible && <Toast variant="success" message="Login Successfully"/>}
+        <div className="container flex flex-col justify-center items-center min-h-screen">
+            <h1 className="text-[2rem] lg:text-[3rem]">Who is Watching?</h1>
+            <Link to="/home" onClick={() => localStorage.removeItem('loggedUser')} className="flex flex-col items-center justify-center group">
+                <img
+                  src={photoURL ? photoURL : "/images/default-red.png"}
+                  alt="profile1"
+                  className="rounded-md w-8 lg:w-10 my-1 lg:my-2 group-hover:border-[2px] group-hover:border-white"
+                />
+                <h2 className="text-light text-[1.1rem] lg:text-[1.5rem] group-hover:text-white ">{name || email}</h2>
+            </Link>
+        </div>
+
+    </div>
+  )
 }
+
+export default Profiles
